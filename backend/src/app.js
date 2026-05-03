d39fa7cb5c88b202b5c878d7c0b837d920496efd
Dabox <alaminlamina5@gmail.com>
2026-05-03 04:15:12 +0100

    Fixing The Fly.io Connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,11 @@ const roleRoutes = require('./routes/roleRoutes');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
